f5c52b8589f24240e4c3ad47abe9de78391605c1
Zhenyu Dong <315300270@qq.com>
2024-01-19 12:05:20 +0800

    Initialization of util

---
+++ sylar/util.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file util.cpp
+ * @brief util函数实现
+ * @version 0.1
+ * @date 2024-01-19
+ */
+
+#include "util.h"
+#include <unistd.h>
+#include <sys/syscall.h>
+
+namespace sylar {
+
+pid_t GetThreadId() {
+    return syscall(SYS_gettid); 
+}
+
+uint64_t GetFiberId() {
+    return 0; 
+}
+} // namespace sylar
---
+++ sylar/util.h
@@ -0,0 +1,30 @@
+/**
+ * @file util.h
+ * @brief util函数
+ * @version 0.1
+ * @date 2024-01-19
+ */
+
+#ifndef __SYLAR_UTIL_H__
+#define __SYLAR_UTIL_H__
+
+#include <sys/types.h>
+#include <stdint.h>
+
+namespace sylar {
+
+/**
+ * @brief 获取线程id
+ * @note 这里不要把pid_t和pthread_t混淆，参考man 2 gettid
+ */
+pid_t GetThreadId();
+
+/**
+ * @brief 获取协程id
+ * @attention 桩函数，暂时返回0，等协程模块完善后再返回实际值
+ */
+uint64_t GetFiberId();
+
+} // namespace sylar
+
+#endif // __SYLAR_UTIL_H__
